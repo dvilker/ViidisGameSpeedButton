74c7262be81424a70e04258417b9d523874e6378
viidi <viidi@yandex-team.ru>
2022-12-15 15:08:06 +0300

    Add auto mode: The maximum speed when traveling by train to a station or point.

--- control.lua
+++ control.lua
@@ -4,13 +4,14 @@ local speeds = {
     0.1, 0.25, 1, 2, 4, 60
 }
 local eps = 0.0001
+local maxSpeed = 60
 
 local function updateButton()
     local caption
     if game.speed > 1 - eps or game.speed < eps then
-        caption = "×" .. tostring(math.floor(game.speed * 100) / 100)
+        caption = "×" .. tostring(math.floor(game.speed * 100) / 100) .. (global.auto and " A" or "")
     else
-        caption = "/" .. tostring(math.floor(100 / game.speed) / 100)
+        caption = "/" .. tostring(math.floor(100 / game.speed) / 100) .. (global.auto and " A" or "")
     end
 
     for _, player in pairs(game.players) do
@@ -30,21 +31,50 @@ local function updateButton()
     end
 end
 
+local speedUpStates = {
+    [defines.train_state.arrive_signal] = true,
+    [defines.train_state.arrive_station] = true,
+    [defines.train_state.on_the_path] = true,
+    [defines.train_state.wait_signal] = true,
+}
+
+---@return number
+local function gameSpeedByState()
+    if #game.connected_players == 1 then
+        local player = game.connected_players[1]
+        if player.vehicle and player.vehicle.train then
+            if speedUpStates[player.vehicle.train.state] then
+                return maxSpeed
+            end
+        end
+    end
+    return 1
+end
+
 ---@param event OnGuiClick
 local function onGuiClick(event)
     if event.element.name == name then
         if event.button == defines.mouse_button_type.left then
-            if game.speed < 1 - eps then
-                game.speed = 1
+            if event.shift then
+                if not game.is_multiplayer() then
+                    global.auto = true
+                    game.speed = gameSpeedByState()
+                end
             else
-                for i = 1, #speeds, 1 do
-                    if speeds[i] > game.speed + eps then
-                        game.speed = speeds[i]
-                        break
+                global.auto = nil
+                if game.speed < 1 - eps then
+                    game.speed = 1
+                else
+                    for i = 1, #speeds, 1 do
+                        if speeds[i] > game.speed + eps then
+                            game.speed = speeds[i]
+                            break
+                        end
                     end
                 end
             end
         elseif event.button == defines.mouse_button_type.right then
+            global.auto = nil
             if game.speed > 1 + eps then
                 game.speed = 1
             else
@@ -56,6 +86,7 @@ local function onGuiClick(event)
                 end
             end
         elseif event.button == defines.mouse_button_type.middle then
+            global.auto = nil
             game.speed = 1
         end
         updateButton()
@@ -76,6 +107,22 @@ local function updateSpeeds()
         end
         if #newSpeeds > 0 then
             speeds = newSpeeds
+            maxSpeed = nil
+            for _, speed in pairs(newSpeeds) do
+                if not maxSpeed or maxSpeed < speed then
+                    maxSpeed = speed
+                end
+            end
+        end
+    end
+end
+
+local function updateAutoSpeed()
+    if global.auto then
+        local newSpeed = gameSpeedByState()
+        if math.abs(game.speed, newSpeed) > eps then
+            game.speed = newSpeed
+            updateButton()
         end
     end
 end
@@ -93,3 +140,6 @@ script.on_event(defines.events.on_runtime_mod_setting_changed, updateSpeeds)
 script.on_init(init)
 script.on_load(init)
 script.on_configuration_changed(init)
+
+script.on_event(defines.events.on_player_driving_changed_state, updateAutoSpeed)
+script.on_event(defines.events.on_train_changed_state, updateAutoSpeed)
